d0d4453bcad167561bb1bf7c0c690c456b9d8f51
Jadyn Englett <jadyn.englett@gmail.com>
2024-11-03 16:19:45 -0500

    committing all files for side enemy4

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,15 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
     public GameObject enemy;
+    public GameObject sideEnemy;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateSideEnemy", 1f, 6f);
+        Debug.Log("Side Enemy");
     }
 
     // Update is called once per frame
@@ -24,5 +27,11 @@ public class GameManager : MonoBehaviour
     void CreateEnemy()
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 8f, 0), Quaternion.identity);
+    }
+
+    void CreateSideEnemy()
+    {
+        Instantiate(sideEnemy, new Vector3(24f, Random.Range(1f, 9f), 0), Quaternion.identity);
+        Debug.Log("Side Enemy Spawned");
     }
 }
